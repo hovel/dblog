ee7d64580cc95d327dd6005e4af39eea3cb3ea40
Vladimir Korsun <korsun.vladimir@gmail.com>
2011-08-08 18:35:16 +0300

    Setup fix.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
 name = 'dblog',
 version = '1.0',
-package_dir = {'dblog': ''},
+package_dir = {'dblog': '.'},
 package_data = {'': ['templates/*/*', 'fixtures/*', 'locale/*/*']},
 packages = ['dblog'],
 )
