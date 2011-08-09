c05c126e464f326d4dc35dabe2d182794e8d2f36
Vladimir Korsun <korsun.vladimir@gmail.com>
2011-08-09 21:10:49 +0300

    Setup script changes.

--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ setup(
 name = 'dblog',
 version = '1.0',
 package_dir = {'dblog': 'dblog'},
-package_data = {'': ['templatetags/*', 'urls/*', 'templates/*/*',
-    'fixtures/*', 'locale/*/*', 'migrations/*']},
+package_data = {'': [
+    'templatetags/*',
+    'urls/*',
+    'templates/*/*',
+    'fixtures/*',
+    'locale/*/*',
+    'migrations/*']},
 packages = ['dblog'],
 )
