ee5e6dcd923c9ecc21250b2b15d252de868e7ff4
Vladimir Korsun <korsun.vladimir@gmail.com>
2011-08-09 16:16:17 +0300

    Fix setup script.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
 name = 'dblog',
 version = '1.0',
 package_dir = {'dblog': '.'},
-package_data = {'': ['urls/*', 'templates/*/*',
+package_data = {'': ['templatetags/*', 'urls/*', 'templates/*/*',
     'fixtures/*', 'locale/*/*', 'migrations/*']},
 packages = ['dblog'],
 )
