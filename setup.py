d0ea9effd999df2d7821949ab0a90003a017b3dd
Vladimir Korsun <korsun.vladimir@gmail.com>
2011-08-09 20:44:20 +0300

    Changed source package structure.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
 name = 'dblog',
 version = '1.0',
-package_dir = {'dblog': '.'},
+package_dir = {'dblog': 'dblog'},
 package_data = {'': ['templatetags/*', 'urls/*', 'templates/*/*',
     'fixtures/*', 'locale/*/*', 'migrations/*']},
 packages = ['dblog'],
