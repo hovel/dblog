e385ebb4f46726a8a2d2c0e01a9be5ccacdbc433
Vladimir Korsun <korsun.vladimir@gmail.com>
2011-08-11 00:28:24 +0300

    Setup changes.

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 setup(
 name = 'dblog',
 version = '1.1',
-package_dir = {'dblog': 'dblog'},
-package_data = {'': [
+packages = find_packages(),
+include_package_data = True,
+package_data = {'dblog': [
     'templatetags/*',
     'urls/*',
     'templates/*/*',
     'fixtures/*',
     'locale/*/*',
     'migrations/*']},
-packages = ['dblog'],
 )
