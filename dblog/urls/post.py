e5726e271d8570424d1c952cd00bdca3aa971840
43FUN <stream-92@mail.ru>
2017-02-07 13:22:07 +0300

    Testproject for "django1.8"

--- dblog/urls/post.py
+++ dblog/urls/post.py
@@ -7,7 +7,7 @@ from dblog.views import *
 urlpatterns = patterns(
     '',
     (r'^$', PostList.as_view(), None, 'promoted'),
-    (r'^feed/$', LatestPostsFeed(), None, 'feed' ),
+    (r'^feed/$', LatestPostsFeed(), None, 'feed'),
     (r'^create/$', PostCreate.as_view(), None, 'create'),
     (r'^(?P<pk>\d+)/$', PostDetail.as_view(), None, 'detail'),
     (r'^(?P<pk>\d+)/update/$', PostUpdate.as_view(), None, 'update'),
